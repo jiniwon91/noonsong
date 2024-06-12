c6bffe7e520ef8d391ebd834d5cfa516f3b1f086
esc <eunso02@sookmyung.ac.kr>
2024-06-13 00:34:24 +0900

    change lambda to cityapi

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -3,11 +3,11 @@ import 'package:http/http.dart' as http;
 import 'package:nunsong/services/map_api.dart';
 
 class ApiService {
-  static const baseurl =
-      'https://ctrtk2ywofaktzy4susqp3s4u40udnlf.lambda-url.us-east-1.on.aws';
+  static const baseurl_place =
+      'https://ubunj66fwhu2yktk4tw4noo77e0fvmrb.lambda-url.ap-northeast-2.on.aws/';
 
   static Future<MapModel> fetchDatafromAPI(String pname) async {
-    final url = Uri.parse('$baseurl/?pname=$pname');
+    final url = Uri.parse('$baseurl_place/?pname=$pname');
     final response = await http.get(url);
     if (response.statusCode == 200) {
       final String responseBody =
