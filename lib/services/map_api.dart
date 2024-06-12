3277beb976a531bc8b1d89eba82d8c8bfde8beee
esc <eunso02@sookmyung.ac.kr>
2024-06-12 16:43:35 +0900

    code change for int and double APIs

--- lib/services/map_api.dart
+++ lib/services/map_api.dart
@@ -1,14 +1,15 @@
 class MapModel {
-  final String location, CONGEST_LVL, CONGEST_MSG, PPLTN_TIME, PCP_MSG;
+  final String location, CONGEST_LVL, CONGEST_MSG, PPLTN_TIME, SKY_STTS;
   final int PPLTN_MIN, PPLTN_MAX;
   final double TEMP;
+
   MapModel.fromJson(Map<String, dynamic> json)
       : location = json['location'],
-        CONGEST_LVL = json['CONGEST_LVL'],
-        CONGEST_MSG = json['CONGEST_MSG'],
-        PPLTN_MIN = json['PPLTN_MIN'],
-        PPLTN_MAX = json['PPLTN_MAX'],
+        CONGEST_LVL = json['AREA_CONGEST_LVL'],
+        CONGEST_MSG = json['AREA_CONGEST_MSG'],
+        PPLTN_MIN = int.parse(json['AREA_PPLTN_MIN'].toString()),
+        PPLTN_MAX = int.parse(json['AREA_PPLTN_MAX'].toString()),
         PPLTN_TIME = json['PPLTN_TIME'],
-        TEMP = json['TEMP'],
-        PCP_MSG = json['PCP_MSG'];
+        TEMP = double.parse(json['TEMP'].toString()),
+        SKY_STTS = json['SKY_STTS'];
 }
