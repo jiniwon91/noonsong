0c5ffa18361a07ff1e2adda2d2bb05868136e081
esc <eunso02@sookmyung.ac.kr>
2024-06-14 03:59:20 +0900

    add cafe, store API

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -8,7 +8,7 @@ class ApiService {
       'https://ubunj66fwhu2yktk4tw4noo77e0fvmrb.lambda-url.ap-northeast-2.on.aws';
   static const baseurl_restaurant =
       'https://zg4skp4oc3q2cfd2ymkml3gcqe0rfedf.lambda-url.ap-northeast-2.on.aws';
-  static const baseurl_convenienceStore =
+  static const baseurl_store =
       'https://ywer37hwy6x3rh3tzqwwwaox2m0mpgqh.lambda-url.ap-northeast-2.on.aws';
   static const baseurl_cafe =
       'https://fandeh63wy3vnm3eoei5xrh3wy0hjooo.lambda-url.ap-northeast-2.on.aws';
@@ -21,7 +21,6 @@ class ApiService {
           const Utf8Decoder().convert(response.bodyBytes);
       final Map<String, dynamic> mapinfo = jsonDecode(responseBody);
       mapinfo['location'] = pname;
-      print(mapinfo);
       return MapModel.fromJson(mapinfo);
     } else {
       throw Exception('Failed to load map data');
@@ -38,10 +37,43 @@ class ApiService {
       final List<dynamic> restaurantsJson = jsonDecode(response.body);
       for (var restaurantJson in restaurantsJson) {
         final restaurant = StoreModel.fromJson(restaurantJson);
-        print(restaurant.name);
         RestaurantInstances.add(restaurant);
       }
       return RestaurantInstances;
+    }
+    throw Error();
+  }
+
+  static Future<List<StoreModel>> getCafe(
+      String latitude, String longitude) async {
+    List<StoreModel> CafeInstances = [];
+    final url =
+        Uri.parse('$baseurl_cafe/?latitude=$latitude&&longitude=$longitude');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final List<dynamic> cafesJson = jsonDecode(response.body);
+      for (var cafeJson in cafesJson) {
+        final cafe = StoreModel.fromJson(cafeJson);
+        CafeInstances.add(cafe);
+      }
+      return CafeInstances;
+    }
+    throw Error();
+  }
+
+  static Future<List<StoreModel>> getStore(
+      String latitude, String longitude) async {
+    List<StoreModel> StoreInstances = [];
+    final url =
+        Uri.parse('$baseurl_store/?latitude=$latitude&&longitude=$longitude');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final List<dynamic> storesJson = jsonDecode(response.body);
+      for (var storeJson in storesJson) {
+        final store = StoreModel.fromJson(storeJson);
+        StoreInstances.add(store);
+      }
+      return StoreInstances;
     }
     throw Error();
   }
